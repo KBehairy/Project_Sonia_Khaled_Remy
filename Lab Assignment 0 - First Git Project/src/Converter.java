49b7ec5aaedadb5ee6f7023b08c932addb205ec8
KBehairy <kbehairy@ucalgary.ca>
2020-09-11 14:47:34 -0600

    adding Converter Java File

---
+++ Lab Assignment 0 - First Git Project/src/Converter.java
@@ -0,0 +1,28 @@
+
+public class Converter {
+	//Sonia, Khaled Behairy, Remy
+	
+	/*
+	 * @Author: Sonia
+	 * 			Khaled Behairy
+	 * 			Remy
+	 */
+
+	
+	private double celsiusToFahrenheit(double C){
+		// TODO: The third student will implement this method
+		return 0;
+	}
+	
+	private double fahrenheitToCelsius(double F){
+		// TODO: The second student will implement this method
+		return 0;
+	}
+	
+	public static void main(String[] args) {
+		//TODO: The first student will implement this method.
+		// Call CelsiusToFahrenheit to convert 180 Celsius to Fahrenheit value.
+		// Call FahrenheitToCelsius to convert 250 Fahrenheit to Celsius value.
+	}
+	
+}
